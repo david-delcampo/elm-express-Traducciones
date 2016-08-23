552373d235b692f1150be98946d0fc3807107b2f
David <david@cincoinfo.com>
2016-08-23 23:13:01 +0200

    [server] Base server express
    Partiendo de nuestro prototipo de proyecto express: https://github.com
    /david-delcampo/node-express-prototype
    
    Aunque no necesitaremos las vistas, solo queremos un API REST.

---
+++ server/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// statis: js,img,css
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use(express.static(path.join(__dirname, './public')));
+
+// routes
+var routes = require('./routes/index');
+app.use('/', routes);
+
+module.exports = app;
