cb0c086f89df2bba5d0b4203dd28d205e6438c2a
David <david@cincoinfo.com>
2016-08-23 14:44:45 +0200

    [app][elm] Importación de las dependencias

--- app/Main.elm
+++ app/Main.elm
@@ -1,1 +1,12 @@
 module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as Html
+import Html.Events exposing (..)
+import Json.Decode as JsD exposing ((:=))
+import Json.Encode as JsE
+import Http
+import Task exposing (Task)
+import Task.Extra exposing (..)
+import Maybe exposing (Maybe)
