70e3e32ca97887b09f8dd2e9869ae93aa412cb55
David <david@cincoinfo.com>
2016-08-23 22:21:23 +0200

    [app][elm] main app con el objeto representativo de la aplicación
    Se unen las piezas antes definidas, para crear la comunicación entre
    ellas: Model, Update, View.
    
    El uso de las subscriptiones, nuevo concepto en la versión 0.17, no lo
    sabemos de momento interpretar y no lo damos uso.

--- app/Main.elm
+++ app/Main.elm
@@ -186,4 +186,8 @@ view model =
     br [] [],
     testos model.traducciones
     ]
-    
+
+
+main = 
+  Html.program 
+    {init = init, update = update, view = view, subscriptions = \_ -> Sub.none}      
